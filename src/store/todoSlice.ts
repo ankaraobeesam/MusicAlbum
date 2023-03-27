e0d80393eca0e54fc09d4424e951dd6aec704253
ankaraobeesam <111894187+ankaraobeesam@users.noreply.github.com>
2023-03-27 23:20:19 +0530

    Added new files: 	.gitignore 	README.md 	package-lock.json 	package.json 	public/ 	src/App.css 	src/App.test.tsx 	src/App.tsx 	src/Components/ 	src/app/ 	src/features/ 	src/hooks/ 	src/index.css 	src/index.tsx 	src/logo.svg 	src/models/ 	src/react-app-env.d.ts 	src/reportWebVitals.ts 	src/service/ 	src/setupTests.ts 	src/store/actionCreators.ts 	src/store/actionTypes.ts 	src/store/index.ts 	src/store/todo-actions.ts 	src/store/todo-slice.ts 	tsconfig.json
    Initial commit

---
+++ src/store/todoSlice.ts
@@ -0,0 +1,5 @@
+import {MusicModel, MusicArrayModel} from "../models/redux-model"
+import {createSlice, PayloadAction} from "@reduxjs/toolkit"
+const initialTodostate: TodoArrayModel={
+all_todos: [],
+particular_todo...
