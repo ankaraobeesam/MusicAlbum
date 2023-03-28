e42ceaabbc97c726fb31c4045acd8383572613a5
ankaraobeesam <111894187+ankaraobeesam@users.noreply.github.com>
2023-03-28 12:36:51 +0530

    Update todoSlice.ts

--- src/store/todoSlice.ts
+++ src/store/todoSlice.ts
@@ -1,5 +1,6 @@
 import {MusicModel, MusicArrayModel} from "../models/redux-model"
 import {createSlice, PayloadAction} from "@reduxjs/toolkit"
-const initialTodostate: TodoArrayModel={
+const initialTodostate={
 all_todos: [],
-particular_todo...
+particular_todo :{}
+}
